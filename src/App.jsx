0e380f41cb76a0477cd713632b6e57c5de40bec5
Aram Avakyan <avakanaram20@gmail.com>
2024-12-24 18:35:35 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Div = styled.div`
+  text-align: center;
+`;
+
+export const App = () => {
+  return (
+    <>
+      <i className="fa fa-camera-retro"></i>
+      <Div>Hello</Div>
+    </>
+  );
+};
